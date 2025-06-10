169f1fa8aa786800562ee5007e8d17a1fa9a47bb
ItalianoBruno <brunoitaliano03@gmail.com>
2025-06-10 19:31:28 -0300

    progress 1

--- main.js
+++ main.js
@@ -21,14 +21,14 @@ const config = {
   physics: {
     default: "arcade",
     arcade: {
-      gravity: { y: 0 },
-      debug: false,
+      gravity: { y: 1200 },
+      debug: true,
     },
   },
   // List of scenes to load
   // Only the first scene will be shown
   // Remember to import the scene before adding it to the list
-  scene: [Game, GameOver, ],
+  scene: [Game, GameOver],
 };
 
 // Create a new Phaser game instance
--- scenes/Game.js
+++ scenes/Game.js
@@ -17,77 +17,72 @@ export default class Game extends Phaser.Scene {
   create() {
 
     //Player
-    this.player = this.physics.add.sprite(spawnX, spawnY, "dude");
-    this.player.setScale(1.25);
-    //Vars del pj
-    this.tieneHacha = false;
-
-    //Colectables
-    this.stars = this.physics.add.group();
-    objectsLayer.objects
-      .filter(obj => obj.name === "colect")
-      .forEach(obj => {
-        const star = this.stars.create(obj.x, obj.y, "star").setOrigin(1.1, 0.25).setScale(1.5);
-        star.body.allowGravity = false;
-      });
-
-    // Overlap para recolectar estrellas
-    this.physics.add.overlap(this.player, this.stars, (player, star) => {
-      star.disableBody(true, true);
-      this.score += 100;
-      this.coleccionados += 1;
-      console.log("Coleccionados: ", this.coleccionados);
-      console.log("Puntaje:", this.score);
-    }, null, this);
-
+    this.player = this.physics.add.sprite(150, 900, "dude");
+    this.player.setScale(2.25);
+    this.player.setCollideWorldBounds(true);
     //Camara
     this.cameras.main.startFollow(this.player);
-    this.cameras.main.setZoom(1.25);
+    this.cameras.main.setZoom(1);
     // Limita la cámara al tamaño del mapa
-    this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+    this.cameras.main.setBounds(0, 0, 1920, 1080);
+
+    //piso
+    this.ground = this.add.tileSprite(0, 1010, 1550, 100, "dude");
+    this.ground.setOrigin(0, 0);
+    this.ground.setScale(2);
+    this.physics.add.existing(this.ground);
+    this.ground.body.setAllowGravity(false);
+    this.ground.body.setImmovable(true);
+    this.physics.add.collider(this.player, this.ground);
 
     //Profundidad de las capas
-    if (pisoLayer) pisoLayer.setDepth(0);
-    if (arbolLayer) arbolLayer.setDepth(2);
-    if (arbol2Layer) arbol2Layer.setDepth(2);
-    this.player.setDepth(1);
+    //this.player.setDepth(1);
 
     //Resumir Teclas
     this.cursors = this.input.keyboard.createCursorKeys();
     this.keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
-    this.interact = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+    this.spaceBar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
 
     // vel pj
-    this.speed = 180;
+    this.speed = 650;
+
+    // Grupo para los objetos enemigos
+    this.enemigos = this.physics.add.group();
+
+    // Intervalo de aparición (en milisegundos)
+    this.tiempoAparicion = Phaser.Math.Between(500, 2500); // Cambia este valor para ajustar el intervalo
+    this.velocidadEnemigo = -300; // Cambia este valor para ajustar la velocidad
+
+    // Evento para crear enemigos periódicamente
+    this.time.addEvent({
+      delay: this.tiempoAparicion,
+      callback: () => {
+        // Crea un enemigo en la posición izquierda, en una altura aleatoria
+        const enemigo = this.enemigos.create(1950, 980, "bomb"); // Usa la imagen que prefieras
+        enemigo.setVelocityX(this.velocidadEnemigo);
+        enemigo.setCollideWorldBounds(false);
+        enemigo.setImmovable(true);
+        enemigo.body.allowGravity = false;
+        enemigo.setScale(1.2);
+      },
+      callbackScope: this,
+      loop: true
+    });
+
+    // Colisión entre jugador y enemigos
+    this.physics.add.overlap(this.player, this.enemigos, () => {
+      // Lógica de muerte: reinicia la escena o ve a GameOver
+      this.scene.restart();
+    }, null, this);
   }
 
   update() {
     
-    if (this.cursors.left.isDown) {
-      this.player.setVelocityX(-this.speed);
-      
-    }
-    if (this.cursors.right.isDown) {
-      this.player.setVelocityX(this.speed);
-      
-    }
-    if (this.cursors.up.isDown) {
+    if (this.spaceBar.isDown) {
       this.player.setVelocityY(-this.speed);
     }
-    if (this.cursors.down.isDown) {
-      this.player.setVelocityY(this.speed);
-    }
-    if (this.cursors.left.isUp && this.cursors.right.isUp && this.cursors.up.isUp && this.cursors.down.isUp) {
+    this.player.setVelocityX(0);
 
-    }
-    if (this.cursors.left.isUp && this.cursors.right.isUp) {
-      // If no cursor keys are pressed, stop the player
-      this.player.setVelocityX(0);
-    }
-    if (this.cursors.up.isUp && this.cursors.down.isUp) {
-      // If no cursor keys are pressed, stop the player
-      this.player.setVelocityY(0);
-    }
     if (Phaser.Input.Keyboard.JustDown(this.keyR)) {
       console.log("Phaser.Input.Keyboard.JustDown(this.keyR)");
       this.scene.restart();
